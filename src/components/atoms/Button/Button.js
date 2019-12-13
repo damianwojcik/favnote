3359d1689160eaecbffc24658198a96d3389ad31
khamian <khamian@gmail.com>
2019-12-13 09:32:41 +0100

    added theme, global variable colors

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
+  background-color: ${({ theme }) => theme.primary};
   width: ${({ width }) => width || '220px'};
   height: 47px;
   border-radius: 50px;
